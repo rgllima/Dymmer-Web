85f55f173029963beb1e5ca54e6eeec8a8ed50eb
Rafael Gonçalves Lima <rglllima@gmail.com>
2018-09-20 00:01:36 -0300

    CSS edition of TreeComponent and HomePage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,24 +1,36 @@
 <template lang="html">
-  <div class="content">
-    <header>
-      <h1>DyMMer Web 2.0</h1>
-    </header>
-    <tree></tree>
+  <div class="homepage-content">
+    <div>
+      <navbar></navbar>
+    </div>
+
+    <div>
+      <tree></tree>
+    </div>
+
+    <footer class="footer has-background-black-bis">
+      <div class="content has-text-centered">
+        <p class="has-text-light">
+          <strong class="has-text-light">DyMMer</strong> by <a href="https://www.quixada.ufc.br/">Universidade Federal do Ceará - Campus Quixadá</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 import Tree from './Tree.vue'
+import Navbar from './Navbar/Navbar.vue'
 
 export default {
   components: {
-    Tree
+    Tree,
+    Navbar
   }
 }
 </script>
 
 <style lang="css">
-  header {
-    text-align: center;
-  }
+
 </style>
--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,7 +1,158 @@
 <template>
-  <div>
-    <div id="tree">
-      <v-treeview v-model="feature_model.feature_tree" :openAll="openAll"></v-treeview>
+  <div id="tree-component" class="column">
+    <div id="tree" class="columns is-desktop">
+
+      <div class="column">
+        <div class="box my-background-color">
+          <h3 class="title is-4 has-text-centered">Feature Diagram</h3>
+          <v-treeview v-model="feature_model.feature_tree" :openAll="openAll"></v-treeview>
+        </div>
+
+        <div id="tree-constraints" class="box my-background-color">
+          <h3 class="title is-4 has-text-centered">Corss-Tree Constraints</h3>
+          <div>
+            <p>Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis ultrices augue. Mauris metus.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+          <div id="tree-info" class="box my-background-color">
+            <h3 class="title is-4 has-text-centered">Feature Information</h3>
+
+            <div class="field is-horizontal">
+              <label class="label">Name your feature model: </label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.name" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Short description of feature model:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.description" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Primary Author:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.creator" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Author's Address:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.address" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Author's Email:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="email" v-model="feature_model.email" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Author's Phone Number:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.phone" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Author's Website:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.website" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Author's Organization:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.organization" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Author's Organization Department:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="text" v-model="feature_model.department" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <label class="label">Date model was created:</label>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="input is-small" type="date" v-model="feature_model.date" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="button-edit" @click="editFeatureInformation">
+              <a class="button is-primary">Edit</a>
+            </div>
+            <div id="button-save-or-cancel" hidden>
+              <a class="button is-danger" @click="cancelEditFeatureInformation">Cancel</a>
+              <a class="button is-success" @click="saveFeatureInformation">Save</a>
+            </div>
+          </div>
+
+          <div id="tree-statistics" class="box my-background-color">
+            <h3 class="title is-4 has-text-centered">Feature Model Statistics</h3>
+            <div><label><i class="fa fa-asterisk"></i> Features: </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> Mandatory: </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> Optional: </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> XOR Groups: </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> OR Groups: </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> Grouped: </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> Cross-Tree Constraints (CTC): </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> CTCT (%): </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> CTC Distinct Vars : </label><span>25</span></div>
+            <div><label><i class="fa fa-asterisk"></i> CTC Clause Density: </label><span>25</span></div>
+          </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -154,45 +305,17 @@ export default {
           }
         ],
          feature_model:{
-            "-name":"Prontuário Eletrônico de Pacientes",
-            "meta":{
-               "data":[
-                  {
-                     "-name":"description",
-                     "#text":"Prontuário Eletrônico de Pacientes"
-                  },
-                  {
-                     "-name":"creator",
-                     "#text":"Bruno"
-                  },
-                  {
-                     "-name":"address"
-                  },
-                  {
-                     "-name":"email",
-                     "#text":"brunogomes3000@gmail.com"
-                  },
-                  {
-                     "-name":"phone"
-                  },
-                  {
-                     "-name":"website"
-                  },
-                  {
-                     "-name":"organization",
-                     "#text":"IFRN"
-                  },
-                  {
-                     "-name":"department"
-                  },
-                  {
-                     "-name":"date"
-                  },
-                  {
-                     "-name":"reference"
-                  }
-               ]
-            },
+            "name":"Prontuário Eletrônico de Pacientes",
+            "description": "Model Prontuário Eletrônico de Pacientes",
+            "creator": "Bruno",
+            "address": "Rua xxx",
+            "email": "brunogomes3000@gmail.com",
+            "phone": "88888888",
+            "website": "www.poow.com",
+            "organization": "IFRN",
+            "department": "Ciencia",
+            "date": "2019-12-20",
+            "reference": "www.wowow.com",
             "feature_tree": [{
                "id":"_r",
                "type":"r",
@@ -274,7 +397,24 @@ export default {
       };
     },
     methods: {
-
+      editFeatureInformation() {
+        document.getElementById('button-edit').style.display = 'none'
+        document.getElementById('button-save-or-cancel').style.display = 'block'
+        let inputs = document.getElementsByClassName('input')
+        for (var i = 0; i < inputs.length; i++) inputs[i].disabled = false
+      },
+      cancelEditFeatureInformation() {
+        document.getElementById('button-edit').style.display = 'block'
+        document.getElementById('button-save-or-cancel').style.display = 'none'
+        let inputs = document.getElementsByClassName('input')
+        for (var i = 0; i < inputs.length; i++) inputs[i].disabled = true
+      },
+      saveFeatureInformation() {
+        document.getElementById('button-edit').style.display = 'block'
+        document.getElementById('button-save-or-cancel').style.display = 'none'
+        let inputs = document.getElementsByClassName('input')
+        for (var i = 0; i < inputs.length; i++) inputs[i].disabled = true
+      }
     },
     components: {
       VTreeview
@@ -283,9 +423,8 @@ export default {
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #tree {
-    max-width: 380px;
-  }
+.my-background-color{
+  background-color: #ecf6ec !important;
+}
 </style>
