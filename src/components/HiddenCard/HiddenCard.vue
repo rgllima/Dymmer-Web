88ced38931cb338bf8f2a12543f81672303dea69
Rafael Gonçalves Lima <rglllima@gmail.com>
2018-10-12 22:13:33 -0300

    component that hides part of the contents of a card

---
+++ src/components/HiddenCard/HiddenCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="columns box-title is-gapless is-mobile">
+            <div class="column">
+                <slot name="titleCard"></slot>
+            </div>
+            <span class="column icon is-1"  @click="activeClass()">
+                <i v-if="isActive" class="fas fa-2x fa-angle-down"></i>
+                <i v-if="!isActive" class="fas fa-2x fa-angle-up"></i>
+            </span>
+        </div>
+
+        <transition name="fade">
+            <slot name="contentCard" v-if="isActive"></slot>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        active: Boolean
+    },
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    methods: {
+        activeClass() {
+            this.isActive = !this.isActive;
+        }
+    },
+    created() {
+        this.isActive = this.active
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active em versões anteriores a 2.1.8 */ {
+    opacity: 0;
+}
+</style>
+
+
