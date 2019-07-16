26a2de077d350649d67bdca038d2f12e0f6feaf8
Rafael Lima <rglllima@gmail.com>
2019-07-16 00:17:39 -0300

    Create feature model is finished

--- src/views/Dashboard/Components/AddFeatureModelModal.vue
+++ src/views/Dashboard/Components/AddFeatureModelModal.vue
@@ -7,17 +7,17 @@
       <div class="card-content">
         <div v-if="!isLoading" class="tile">
           <div class="tile is-parent">
-            <button class="box">
-              <img src="../../../assets/edit-tools.svg" alt="settings">
-              <br>
+            <button class="box button" @click="createFeatureModel">
+              <img src="../../../assets/edit-tools.svg" alt="settings" />
+              <br />
               <p class="has-text-centered">Create Feature Model</p>
             </button>
           </div>
           <div class="tile is-parent">
-            <button class="box">
-              <img src="../../../assets/import.svg" alt="settings">
-              <br>
-              <p class="has-text-centered">Import XML Feature Model</p>
+            <button class="box button">
+              <img src="../../../assets/import.svg" alt="settings" />
+              <br />
+              <p class="has-text-centered">Import XML</p>
               <input
                 class="file-input"
                 type="file"
@@ -25,13 +25,13 @@
                 id="getXmlToString"
                 ref="MyXMLFile"
                 @change="getXmlToString"
-              >
+              />
             </button>
           </div>
         </div>
 
         <div v-else class="card-animation">
-          <lottie :options="defaultOptions" :height="220" :width="220"/>
+          <lottie :options="defaultOptions" :height="220" :width="220" />
           <h4>Converting!</h4>
           <p>Wait a moment...</p>
         </div>
@@ -43,6 +43,7 @@
 <script>
 import Lottie from "vue-lottie";
 import * as animationData from "@/assets/search-file-animation.json";
+import CreateFeatureModel from "./CreateFeatureModalForm";
 
 export default {
   components: {
@@ -65,6 +66,15 @@ export default {
         this.$emit("close");
       };
       reader.readAsText(file);
+    },
+
+    createFeatureModel() {
+      this.$modal.open({
+        parent: this,
+        component: CreateFeatureModel,
+        hasModalCard: true
+      });
+      this.$emit("close");
     }
   }
 };
---
+++ src/views/Dashboard/Components/CreateFeatureModalForm.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="create-feature-modal modal-card box">
+    <div class="subtitle has-text-centered">
+      <h1>Feature Informations</h1>
+      <p class="help is-danger">Red fields are required!</p>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">Feature Model Name</label>
+          <p class="control is-expanded">
+            <input
+              class="input is-danger"
+              type="text"
+              placeholder="Feature Model Name"
+              v-model="fModel.name"
+              required
+            />
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">Author</label>
+          <p class="control is-expanded">
+            <input
+              class="input is-danger"
+              type="text"
+              placeholder="Author"
+              v-model="fModel.creator"
+            />
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="field">
+        <label class="label">Description</label>
+        <p class="control is-expanded">
+          <textarea
+            class="textarea is-danger"
+            placeholder="Write a brief description of the Feature Model..."
+            v-model="fModel.description"
+          ></textarea>
+        </p>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">Email</label>
+          <p class="control is-expanded">
+            <input class="input is-danger" type="email" placeholder="Email" v-model="fModel.email" />
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">Phone</label>
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Phone" v-model="fModel.phone" />
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">Address</label>
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Address" v-model="fModel.address" />
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">Website</label>
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Website" v-model="fModel.website" />
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-vertical">
+      <div class="field">
+        <label class="label">Organization</label>
+        <p class="control is-expanded">
+          <input class="input" type="text" placeholder="Organization" v-model="fModel.organization" />
+        </p>
+      </div>
+      <div class="field">
+        <label class="label">Department</label>
+        <p class="control is-expanded">
+          <input class="input" type="text" placeholder="Department" v-model="fModel.department" />
+        </p>
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <p class="control">
+        <a class="button is-danger" @click="$emit('close')">Cancel</a>
+      </p>
+      <p class="control">
+        <button class="button is-primary" @click="createFModel">Create Feature Model</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+ <script>
+export default {
+  data() {
+    return {
+      fModel: {
+        name: "",
+        description: "",
+        creator: "",
+        address: "",
+        email: "",
+        phone: "",
+        website: "",
+        organization: "",
+        department: "",
+        date: new Date().toISOString(),
+        reference: "",
+        feature_tree: [
+          {
+            id: "_r",
+            type: "r",
+            name: "",
+            children: []
+          }
+        ],
+        constraints: [],
+        contexts: [],
+        origin: "DYMMER",
+        type: "SPL",
+        number_of_features: 0
+      }
+    };
+  },
+
+  methods: {
+    async createFModel() {
+      console.log(this.fModel);
+      let hasIssues = this.validateFModel();
+
+      if (hasIssues) return this.notifyUser(hasIssues);
+
+      this.fModel.feature_tree[0].name = this.fModel.name;
+      await this.$store.commit("featureModel/setFeatureModel", this.fModel);
+      this.$store.commit("featureModel/setHasChanged", true);
+      this.$emit("close");
+      this.$router.push("/fmodel-manager");
+    },
+
+    validateFModel() {
+      if (!this.fModel.name.trim())
+        return "Field 'Feature Model Name' is required!";
+      else if (!this.fModel.creator.trim())
+        return "Field 'Author' is required!";
+      else if (!this.fModel.description.trim())
+        return "Field 'Description' is required!";
+      else if (!this.fModel.email.trim()) return "Field 'Email' is required!";
+      else if (!this.validateEmail(this.fModel.email))
+        return "Field 'Email' is invalid!";
+      return false;
+    },
+
+    validateEmail(email) {
+      let emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return emailRegex.test(email);
+    },
+
+    notifyUser(message) {
+      this.$toast.open({
+        message: message,
+        type: "is-danger"
+      });
+    }
+  },
+
+  mounted() {
+    console.log(this.fModel);
+  }
+};
+</script>
+
+<style lang="sass">
+.modal
+  z-index: 100
+.animation-content
+  min-height: 300px
+.create-feature-modal
+  overflow-y: scroll
+</style>
