fb7b59466d24304a090fe5114614bfd39204a291
Rafael Lima <rglllima@gmail.com>
2021-06-03 17:12:30 -0300

    Changed Deploy File

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -e
 
-eval 'ssh-agent -s'
+eval `ssh-agent -s`
 ssh-add github-dymmer #Add Private Key Every Build - It's a bug im my PC
 
 yarn build
@@ -10,6 +10,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M master
 
 git push -f git@github.com:dymmerufc/dymmerufc.github.io.git master
 
